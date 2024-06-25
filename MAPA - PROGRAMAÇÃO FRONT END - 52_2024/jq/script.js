2c671e9b76a3410820c9082f7d90201d6bdb1eab
AlekssandherMax <alekssandher1@hotmail.com>
2024-06-25 14:41:03 -0300

    terminada o mapa

--- MAPA - PROGRAMAÇÃO FRONT END - 52_2024/jq/script.js
+++ MAPA - PROGRAMAÇÃO FRONT END - 52_2024/jq/script.js
@@ -1,39 +1,30 @@
 $(document).ready(function() {
     $('.form').submit(function(event) {
-        // Verifica se todos os campos obrigatórios estão preenchidos
         if (this.checkValidity()) {
-            // Impede o envio automático do formulário
             event.preventDefault();
 
-            // Simula um envio assíncrono do formulário (você pode substituir isso pela lógica real de envio para o servidor)
             setTimeout(function() {
-                // Exibe mensagem de sucesso
                 showSuccessMessage();
 
-                // Redireciona para a página de login após 3 segundos
                 setTimeout(function() {
-                    window.location.href = "login.html";
-                }, 3000); // Tempo em milissegundos antes de redirecionar (3 segundos aqui)
+                    window.location.href = "../index.html";
+                }, 3000); 
 
-                // Limpa os campos do formulário (opcional)
                 $('.form')[0].reset();
 
-            }, 1000); // Tempo em milissegundos antes de exibir a mensagem (1 segundo aqui)
+            }, 1000); 
         }
     });
 });
 
 function showSuccessMessage() {
-    // Cria um elemento div para a mensagem
-    var messageElement = $('<div class="success-message">Cadastro realizado com sucesso!</div>');
+    var messageElement = $('<div class="success-message">Operação concluida...</div>');
 
-    // Adiciona o elemento à página dentro do main (ou onde desejar)
     $('main').append(messageElement);
 
-    // Após alguns segundos, remove a mensagem
     setTimeout(function() {
         messageElement.fadeOut('slow', function() {
             $(this).remove();
         });
-    }, 3000); // Tempo em milissegundos para manter a mensagem (3 segundos aqui)
+    }, 3000); 
 }
